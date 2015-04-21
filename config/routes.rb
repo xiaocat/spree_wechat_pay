20339f682994ff8b9faa506f2ef152a18c09c112
xiao.han <hanxiao0306@gmail.com>
2015-04-21 18:05:12 +0800

    兼容api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Spree::Core::Engine.routes.draw do
   # Add your extension routes here
   get '/wechatpay/checkout', :to => "wechatpay#checkout"
-  get '/wechatpay/query', :to => "wechatpay#query"
+  get '/wechat/:id/checkout', :to => "wechatpay#checkout_api"
+  post '/wechat/:id/query', :to => "wechatpay#query"
   post '/wechatpay/notify', :to => "wechatpay#notify"
 end
